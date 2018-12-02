c2617180202b1350041309aa80b94a965601cb70
Evan Bonsignori <evanbonsignori@yahoo.com>
2018-12-01 19:43:44 -0500

    Add Steepest Descent and Conjugate Gradient methods

--- iterative_methods.py
+++ iterative_methods.py
@@ -1,11 +1,11 @@
-from _decimal import Decimal
+import math
+from decimal import Decimal
 import numpy as np
-
 
 default_max_iterations = 999
 
 
-def jacobi(a, b, n=None, x=None, delta=None, actual=None, max_iterations=default_max_iterations):
+def jacobi(a, b, n=None, x=None, delta=None, actual=np.array([]), max_iterations=default_max_iterations):
     """
     Solves the equation Ax=b via the Jacobi iterative method.
 
@@ -57,7 +57,7 @@ def jacobi(a, b, n=None, x=None, delta=None, actual=None, max_iterations=default
     return [x, n]
 
 
-def gauss_seidel(a, b, n=None, x=None, delta=None, actual=None, max_iterations=default_max_iterations):
+def gauss_seidel(a, b, n=None, x=None, delta=None, actual=np.array([]), max_iterations=default_max_iterations):
     """
     Solves the equation Ax=b via the Gauss-Seidel iterative method.
 
@@ -106,7 +106,7 @@ def gauss_seidel(a, b, n=None, x=None, delta=None, actual=None, max_iterations=d
     return [x, n]
 
 
-def successive_over_relaxation(a, b, w=1, n=None, x=None, delta=None, actual=None, max_iterations=default_max_iterations):
+def successive_over_relaxation(a, b, w=1, n=None, x=None, delta=None, actual=np.array([]), max_iterations=default_max_iterations):
     """
     Solves the equation Ax=b via the Successive Over Relaxation iterative method.
 
@@ -149,6 +149,102 @@ def successive_over_relaxation(a, b, w=1, n=None, x=None, delta=None, actual=Non
             # the number of iterations it took to get within the error
             if abs(Decimal(actual_norm) - Decimal(x_norm)) <= delta or n >= max_iterations:
                 break
+    # If neither N or delta was passed in
+    else:
+        raise SyntaxError("Must pass in either N or delta options to function")
+
+    # Return the result and the number of iterations taken to find it
+    return [x, n]
+
+
+def steepest_descent(a, b, n=None, x=None, delta=None, actual=np.array([]), max_iterations=default_max_iterations):
+    # Make sure that both delta and actual are passed in
+    if (delta and not actual.any()) or (actual.any() and not delta):
+        raise SyntaxError("Must pass in both delta and actual if one is passed in")
+    # Make sure that only N or delta is passed in
+    if delta and n:
+        raise SyntaxError("Can only pass delta or N option")
+    # Create an initial guess if needed
+    if x is None:
+        x = np.zeros(len(a[0]))
+
+    # Iterate for N times if N is passed in
+    if n:
+        for i in range(n):
+            r = b - np.dot(a, x)  # Residual
+            w = (np.dot(np.transpose(r), r)) / np.dot(np.dot(np.transpose(r), a), r)  # Weight
+            x = x + (w * r)  # approximated solution at current iteration
+
+    # Iterate until error is found or max_iterations is exceeded if delta and actual are passed in
+    elif delta and actual.any():
+        n = 0
+        actual_norm = np.linalg.norm(actual)
+        while True:
+            r = b - np.dot(a, x)  # Residual
+            w = (np.dot(np.transpose(r), r)) / np.dot(np.dot(np.transpose(r), a), r)  # Weight
+            x = x + (w * r)  # approximated solution at current iteration
+
+            x_norm = np.linalg.norm(x)
+            n += 1
+            # Compare norms of actual matrix with steepest-descent calculated matrix and if difference is within error,
+            # return the number of iterations it took to get within the error
+            if abs(Decimal(actual_norm) - Decimal(x_norm)) <= delta or n >= max_iterations:
+                break
+    # If neither N or delta was passed in
+    else:
+        raise SyntaxError("Must pass in either N or delta options to function")
+
+    # Return the result and the number of iterations taken to find it
+    return [x, n]
+
+
+def conjugate_gradient(a, b, n=None, x=None, delta=None, actual=np.array([]), max_iterations=default_max_iterations):
+    # Make sure that both delta and actual are passed in
+    if (delta and not actual.any()) or (actual.any() and not delta):
+        raise SyntaxError("Must pass in both delta and actual if one is passed in")
+    # Make sure that only N or delta is passed in
+    if delta and n:
+        raise SyntaxError("Can only pass delta or N option")
+    # Create an initial guess if needed
+    if x is None:
+        x = np.zeros(len(a[0]))
+
+    r = b - np.dot(a, x)  # Initial Residual
+    p = r
+    rs_old = np.dot(r.T, r)
+
+    # Iterate for N times if N is passed in
+    if n:
+        for i in range(n):
+            Ap = np.dot(a, p)
+            w = rs_old / np.dot(p.T, Ap)
+            x = x + np.dot(w, p)  # approximated solution at current iteration
+
+            r = r - np.dot(w, Ap)
+            rs_new = np.dot(r.T, r)
+            if math.sqrt(rs_new) < delta:
+                break
+
+            p = r + np.dot((rs_new / rs_old), p)
+            rs_old = rs_new
+
+    # Iterate until error is found or max_iterations is exceeded if delta and actual are passed in
+    elif delta and actual.any():
+        n = 0
+        while True:
+            Ap = np.dot(a, p)
+            w = rs_old / np.dot(p.T, Ap)
+            x = x + np.dot(w, p)  # approximated solution at current iteration
+
+            r = r - np.dot(w, Ap)
+            rs_new = np.dot(r.T, r)
+
+            n += 1
+            if math.sqrt(rs_new) < delta:
+                break
+
+            p = r + np.dot((rs_new / rs_old), p)
+            rs_old = rs_new
     # If neither N or delta was passed in
     else:
         raise SyntaxError("Must pass in either N or delta options to function")
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import numpy as np
-from math import sqrt
-from iterative_methods import jacobi, gauss_seidel, successive_over_relaxation
+from iterative_methods import jacobi, gauss_seidel, successive_over_relaxation, steepest_descent, conjugate_gradient
 from utility import print_results
 
+# A = np.array([
+#    [-1, 0, 0, sqrt(2)/2, 1, 0, 0, 0],
+#    [0, -1, 0, sqrt(2)/2, 0, 0, 0, 0],
+#    [0, 0, -1, 0, 0, 0, 1/2, 0],
+#    [0, 0, 0, -sqrt(2)/2, 0, -1, -1/2, 0],
+#    [0, 0, 0, 0, -1, 0, 0, 1],
+#    [0, 0, 0, 0, 0, 1, 0, 0],
+#    [0, 0, 0, -sqrt(2)/2, 0, 0, sqrt(3)/2, 0],
+#    [0, 0, 0, 0, 0, 0, sqrt(3)/2, -1]
+# ])
+#
+# b = np.array([0, 0, 0, 0, 0, 10000, 0, 0])
+
 A = np.array([
-   [-1, 0, 0, sqrt(2)/2, 1, 0, 0, 0],
-   [0, -1, 0, sqrt(2)/2, 0, 0, 0, 0],
-   [0, 0, -1, 0, 0, 0, 1/2, 0],
-   [0, 0, 0, -sqrt(2)/2, 0, -1, -1/2, 0],
-   [0, 0, 0, 0, -1, 0, 0, 1],
-   [0, 0, 0, 0, 0, 1, 0, 0],
-   [0, 0, 0, -sqrt(2)/2, 0, 0, sqrt(3)/2, 0],
-   [0, 0, 0, 0, 0, 0, sqrt(3)/2, -1]
+   [4, 3, 0],
+   [3, 4, -1],
+   [0, -1, 4]
 ])
 
-b = np.array([0, 0, 0, 0, 0, 10000, 0, 0])
+b = np.array([24, 30, -24])
 
 # Get the real solution using built in numpy method and solution using 3 iterative methods within 10^-8 error
 # x_solved = np.linalg.solve(A, b) # Either this of dot product of A inv b works, but the second is more clear
@@ -22,20 +29,30 @@ x_solved = np.dot(np.linalg.inv(A), b)
 x_jac = jacobi(A, b, delta=10**-8, actual=x_solved)
 x_gauss_seidel = gauss_seidel(A, b, delta=10**-8, actual=x_solved)
 x_sor = successive_over_relaxation(A, b, w=1.25, delta=10**-8, actual=x_solved)
+x_steepest_descent = steepest_descent(A, b, delta=10**-8, actual=x_solved)
+x_conjugate_gradient = conjugate_gradient(A, b, delta=10**-8, actual=x_solved)
 
 # Print the results for each method
 print("x [Solved with built in Numpy functions]: ")
 print_results(x_solved)
-print()  # Newline
+print("")  # Newline
 
 print("x [Solved using {} iterations of the Jacobi method]: ".format(x_jac[1]))
 print_results(x_jac[0])
-print()
+print("")
 
 print("x [Solved using {} iterations of the Gauss-Seidel method]: ".format(x_gauss_seidel[1]))
 print_results(x_gauss_seidel[0])
-print()
+print("")
 
 print("x [Solved using {} iterations of the Successive Over Relation (SOR) method]: ".format(x_sor[1]))
 print_results(x_sor[0])
-print()
+print("")
+
+print("x [Solved using {} iterations of the Steepest Descent method]: ".format(x_steepest_descent[1]))
+print_results(x_steepest_descent[0])
+print("")
+
+print("x [Solved using {} iterations of the Conjugate Gradient method]: ".format(x_conjugate_gradient[1]))
+print_results(x_conjugate_gradient[0])
+print("")
